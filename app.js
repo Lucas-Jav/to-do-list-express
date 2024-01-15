af685ef15ec40392917ab30c3a370a9ade71454f
Lucas-Jav <lucas.meialua@outlook.com>
2024-01-15 16:58:37 -0300

    :fire: fix(docs): remove node_modules

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const path = require('path');
+
 const checklistsRouter = require('./src/routes/checklist.js');
 const rootRouter = require('./src/routes/Index.js');
+const methodOverride = require('method-override');
+
+
 require('./config/database.js');
-const path = require('path');
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'));
 
 app.set("views", path.join(__dirname, 'src/views'));
 app.set('view engine', 'ejs')
